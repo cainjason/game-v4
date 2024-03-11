039871d81dd2c87fcb99c26ed8afc6c12e2a80fc
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-11 02:21:21 -0400

    Chore: house keeping in app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h2 className='bg-red-400 h-30 w-full text-3xl'> Test </h2>
+    </div>
+  );
+}
+
+export default App;
